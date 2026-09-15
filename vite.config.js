94d47ac3de62c64b76c05778e580660d3c3c9b27
sanjayvs19 <sanjayvss2045@gmail.com>
2026-09-15 22:50:33 +0530

    Added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: false
+
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['healthsnap-8l0v.onrender.com']
   }
-});
+})
